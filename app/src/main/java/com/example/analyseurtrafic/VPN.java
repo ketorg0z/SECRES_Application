69757990a9108ee852af0856a9373aac274964cc
Lev Savolskiy <lev.savolskiy@etu.sorbonne-universite.fr>
2025-12-05 15:37:48 +0100

    Integrated pcap4j to decode IPv4 and IPv6 packets - addresses, transport layer protocol detection (UDP and TCP) and port number Dependencies and imports to check Application functionality to check

--- app/src/main/java/com/example/analyseurtrafic/VPN.java
+++ app/src/main/java/com/example/analyseurtrafic/VPN.java
@@ -10,6 +10,18 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.pcap4j.packet.IpPacket;
+import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.IpV6Packet;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.TcpPacket;
+import org.pcap4j.packet.UdpPacket;
+import org.pcap4j.packet.namednumber.IpNumber;
+import org.pcap4j.packet.factory.PacketFactories;
+import org.pcap4j.packet.factory.PacketFactory;
+import org.pcap4j.packet.factory.PacketFactoryBinder;
+
+
 public class VPN extends VpnService {
 
     public static final String ACTION_PACKET_RECEIVED = "com.example.analyseurtrafic.PACKET_RECEIVED";
@@ -61,17 +73,48 @@ public class VPN extends VpnService {
         while (true) {
             int length = in.read(packet);
             if (length > 0) {
+                String packetInfo;
+                try {
+                    byte[] capturedPacketBytes = new byte[length];
+                    System.arraycopy(packet, 0, capturedPacketBytes, 0, length);
+                    // 2. Use pcap4j's factory to create a Packet object from the raw bytes.
+                    Packet parsedPacket = PacketFactories.getFactory(Packet.class, IpNumber.class)
+                            .newInstance(capturedPacketBytes, 0, capturedPacketBytes.length, IpNumber.IPV4);
 
-                String packetInfo = "Received packet of size: " + length;
+                    IpPacket ipPacket = parsedPacket.get(IpPacket.class);
+                    if (ipPacket != null) {
+                        String protocol = ipPacket.getHeader().getProtocol().name();
+                        String srcAddr = ipPacket.getHeader().getSrcAddr().getHostAddress();
+                        String dstAddr = ipPacket.getHeader().getDstAddr().getHostAddress();
+                        String portInfo = "";
+
+                        if (protocol.equals("TCP")) {
+                            TcpPacket tcpPacket = ipPacket.get(TcpPacket.class);
+                            int srcPort = tcpPacket.getHeader().getSrcPort().valueAsInt();
+                            int dstPort = tcpPacket.getHeader().getDstPort().valueAsInt();
+                            portInfo = " | " + srcPort + " -> " + dstPort;
+                        } else if (protocol.equals("UDP")) {
+                            UdpPacket udpPacket = ipPacket.get(UdpPacket.class);
+                            int srcPort = udpPacket.getHeader().getSrcPort().valueAsInt();
+                            int dstPort = udpPacket.getHeader().getDstPort().valueAsInt();
+                            portInfo = " | " + srcPort + " -> " + dstPort;
+                        }
+
+                        packetInfo = protocol + " | " + srcAddr + " -> " + dstAddr + portInfo;
+
+                    } else {
+                        packetInfo = "Non-IP Packet - Size: " + length;
+                    }
+
+                } catch (Exception e) {
+                    packetInfo = "Malformed Packet - Size: " + length;
+                    Log.e(TAG, "Error parsing packet", e);
+                }
 
                 Intent intent = new Intent(ACTION_PACKET_RECEIVED);
                 intent.putExtra(EXTRA_PACKET_INFO, packetInfo);
                 LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
 
-                // Here you would parse the packet headers and analyze the data
-                Log.d(TAG, "Received packet of size: " + length);
-                // For a simple start, you can just write the packet back to the interface
-                // to allow traffic to flow.
                 out.write(packet, 0, length);
             }
         }
